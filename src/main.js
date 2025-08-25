1463d515bc4cf7a416e1742deeaba10e4a4b36c9
dev_vens <vensalex1991@gmail.com>
2025-08-25 12:21:17 +0200

    Update stylesheet import to optimized version for improved performance

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import './styles.css'
+import './styles-optimized.css'
 
 import App from './App.vue'
 import router from './router'
