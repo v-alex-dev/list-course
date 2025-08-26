629325c4259b724b95ea2b264c2f5a3121997f95
dev_vens <vensalex1991@gmail.com>
2025-08-26 15:52:48 +0200

    feat: add PWA support with vite-plugin-pwa and configure caching strategies

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,72 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'easysholi',
+        short_name: 'easysholi',
+        description: 'Une app pour pouvoir gérer une liste de course facilement',
+        display: 'standalone',
+        start_url: '/index.html',
+        scope: '/',
+        background_color: '#0f172a', // couleur de fond de l'écran de lancement
+        theme_color: '#06b6d4', // couleur de la barre d'adresse
+        orientation: 'portrait',
+        icons: [
+          {
+            src: '/icons/favicon-16x16.png',
+            sizes: '16x16',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/favicon-32x32.png',
+            sizes: '32x32',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/favicon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/favicon-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        cleanupOutdatedCaches: true,
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/easysholi\.netlify\.app\/.*$/,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'easysholi-cache',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 jours
+              },
+            },
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
