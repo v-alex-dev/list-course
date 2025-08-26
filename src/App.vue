f34a172e9f96fbe3fc38dacac125a7065e27edce
dev_vens <vensalex1991@gmail.com>
2025-08-26 15:08:23 +0200

    Add PwaInstall component to App.vue for PWA integration

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import PwaInstall from './components/PwaInstall.vue'
 </script>
 
 <template>
+  <PwaInstall />
   <main>
     <RouterView />
   </main>
